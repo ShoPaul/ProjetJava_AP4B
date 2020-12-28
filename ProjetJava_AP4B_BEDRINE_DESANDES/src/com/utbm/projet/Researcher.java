29f19e3117e394f3f14ebd0ffc3959bd68732e24
tbedrine <57362627+tbedrine@users.noreply.github.com>
2020-12-28 16:56:59 +0100

    Update Researcher.java

--- ProjetJava_AP4B_BEDRINE_DESANDES/src/com/utbm/projet/Researcher.java
+++ ProjetJava_AP4B_BEDRINE_DESANDES/src/com/utbm/projet/Researcher.java
@@ -1,39 +1,17 @@
+package com.utbm.projet;
 
 import java.util.*;
 
-/**
- * 
- */
 public class Researcher extends Faction {
 
-    /**
-     * Default constructor
-     */
+    private String name;
+    private int nbBoost;
+    private String domain;
+    
     public Researcher() {
     }
 
-    /**
-     * 
-     */
-    private String name;
-
-    /**
-     * 
-     */
-    private int nbBoost;
-
-    /**
-     * 
-     */
-    private String domain;
-
-    /**
-     * @param factionName 
-     * @param boostNumber 
-     * @param boostDomain
-     */
     public void Researcher(String factionName, int boostNumber, String boostDomain) {
         // TODO implement here
     }
-
 }
