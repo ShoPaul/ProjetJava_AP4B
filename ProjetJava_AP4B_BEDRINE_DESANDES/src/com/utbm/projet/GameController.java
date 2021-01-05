a3c1be10d9e642804803c458d4074cbbc73802aa
tbedrine <57362627+tbedrine@users.noreply.github.com>
2021-01-05 16:06:24 +0100

    Create colony method started

--- ProjetJava_AP4B_BEDRINE_DESANDES/src/com/utbm/projet/GameController.java
+++ ProjetJava_AP4B_BEDRINE_DESANDES/src/com/utbm/projet/GameController.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class GameController {
 
-    public GameController() {
+    GameController() {
     }
 
     public void startGame() {
@@ -25,6 +25,29 @@ public class GameController {
 
     public void saveGame() {
         // TODO implement here
+    }
+    
+    public static boolean createColony (int [] costs, String name, Economy eco) {
+    	int [] resources = {eco.carboneNumber,eco.diazoteNumber,eco.hydrogeneNumber,eco.oxygeneNumber};
+    	boolean enoughResources = true;
+    	for (int i = 0; i <4; i++) {
+    		if (costs [i] > resources [i]) {
+    			enoughResources = false;
+    		}
+    	}
+    	//Warning ! The list costs must contain needed resources in the following order : carbon, diazote, hydrogen, oxygen
+    	if (enoughResources) {
+    		eco.carboneNumber -= costs [0];
+    		eco.diazoteNumber -= costs [1];
+    		eco.hydrogeneNumber -= costs [2];
+    		eco.oxygeneNumber -= costs [3];
+    		
+    		return true;
+    	} else {
+    		System.out.println ("Insufficient resources, you cannot create a new colony");
+    		return false;
+    	}
+    	
     }
 
 }
